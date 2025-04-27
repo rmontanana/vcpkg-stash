62637649469f5ef78735cf28a2b3cd836c68f380
Ricardo Montañana Gómez <rmontanana@gmail.com>
2025-04-27 17:13:08 +0200

    Change install way of folding

--- ports/folding/portfile.cmake
+++ ports/folding/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 0c86f58e0cb66349bb4e141e3e32a205a264daef6ffbd6284f76a2bb4deb303f752eb85954cd78082900a356da1940e1cf74c9732752b208f5dbf36f28ecfd2e
     HEAD_REF main
+    GITHUB_HOST https://gitea.rmontanana.es
 )
 
 # This is a header only library
@@ -11,3 +12,7 @@ file(INSTALL "${SOURCE_PATH}/folding.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/i
 
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+vcpkg_install_cmake_config(
+    NAMESPACE folding
+)

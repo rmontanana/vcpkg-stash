df83599a3d793a4e227cd7f65b3794441c4526ff
Ricardo Montañana Gómez <rmontanana@gmail.com>
2025-04-27 12:59:18 +0200

    Change bayesnet url to _vcpkg

--- ports/bayesnet/portfile.cmake
+++ ports/bayesnet/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO doctorado-ml/bayesnet
+    REPO rmontanana/bayesnet_vcpkg
     REF "v${VERSION}"
     SHA512 25a723a5f83184b78cdb5f8dd7de84ae91c37b54c04dd44d259ceac0cb1a290dbab04add6902eca6cb826302d3259d5e3a06c1e53cba1b0f7523802ad29ce0ba
     HEAD_REF main

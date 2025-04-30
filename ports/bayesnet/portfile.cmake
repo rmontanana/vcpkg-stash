87766122a358423bf2b3e83d8f209e0d52bd56c4
Ricardo Montañana Gómez <rmontanana@gmail.com>
2025-04-30 10:31:28 +0200

    change bayesnet repo name

--- ports/bayesnet/portfile.cmake
+++ ports/bayesnet/portfile.cmake
@@ -2,9 +2,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO rmontanana/bayesnet
+    REPO doctorado-ml/bayesnet_vcpkg
     REF "v${VERSION}"
-    SHA512 245e3ad1e7e1d7c9657230a8e77622284b073646f6e877c9226efeeefec43fc915e23b9ee7b19510c94b5622b3b777bcc3fb57f44d2c6181599fb9c5381e6c45
+    SHA512 0
     HEAD_REF main
     GITHUB_HOST https://gitea.rmontanana.es
 )
@@ -18,9 +18,7 @@ set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME "bayesnet")
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mytorchlib)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tests")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

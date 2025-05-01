73a23b77530d04955247f3396ea721c9ab0ed7ed
Ricardo Montañana <rmontanana@gmail.com>
2025-05-01 10:22:19 +0200

    update sha

--- ports/bayesnet/portfile.cmake
+++ ports/bayesnet/portfile.cmake
@@ -1,4 +1,6 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 set(VCPKG_BUILD_TYPE Release)
 
@@ -14,11 +16,16 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DCMAKE_CXX_STANDARD=17
+        -DCMAKE_BUILD_TYPE=Release
 )
 set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "bayesnet")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "bayesnet" CONFIG_PATH lib/cmake/bayesnet)
+vcpkg_cmake_config_fixup()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tests")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")

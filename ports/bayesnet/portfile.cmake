e98987cb0ffe945f9d2881bb35ce9903f5347940
Ricardo Montañana Gómez <rmontanana@gmail.com>
2025-05-01 13:19:57 +0200

    Update sha

--- ports/bayesnet/portfile.cmake
+++ ports/bayesnet/portfile.cmake
@@ -1,33 +1,26 @@
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
-set(VCPKG_BUILD_TYPE Release)
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO rmontanana/bayesnet_vcpkg
+    REPO rmontanana/mdlp
     REF "v${VERSION}"
-    SHA512 77e3071835bc3a7dce6a3f4289625ce640564049ce4c03c3c9ab21cf134af00deec67708c1e243ead1c210b85ac0f3e983e052ba9a6948212babee3c146d7909
+    SHA512 762da4a81c98a07f092fbd01864f39786b70f7cfc9a643329b954e1fb6edca0a068a5f50696e72c2f8a24ead95a0d011406bcaca7ba35b1b01a2f19c18d7154e
     HEAD_REF main
-    GITHUB_HOST https://gitea.rmontanana.es
 )
 
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DCMAKE_CXX_STANDARD=17
-        -DCMAKE_BUILD_TYPE=Release
 )
+set(VCPKG_BUILD_TYPE release)
 set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "bayesnet" CONFIG_PATH lib/cmake/bayesnet)
-vcpkg_cmake_config_fixup()
-vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "bayesnet")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mytorchlib)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tests")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tests/lib")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
